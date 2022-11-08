cb5ffc8f7ec586962ac5e416527b90961cbb4967
dongeun <dongeun@dongeunui-MacBookPro.local>
2022-11-08 21:33:02 +0900

    프로젝트 생성

---
+++ src/test/java/dongeun/TripleApplicationTests.java
@@ -0,0 +1,13 @@
+package dongeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
